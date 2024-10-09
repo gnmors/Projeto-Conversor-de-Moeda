05c0375c77807611093933a7c031a488d78acbc4
Giovanni Morais <giovannimo1@yahoo.com>
2024-10-09 16:48:05 -0300

    Iniciando projeto

---
+++ src/br/com/alura/conversor/MinhaAPI.java
@@ -0,0 +1,15 @@
+package br.com.alura.conversor;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+public class MinhaAPI {
+
+
+
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("https://v6.exchangerate-api.com/v6/76bafad47950f991dc9a2b32/latest/"))
+            .build();
+}
---
+++ src/br/com/alura/conversor/Moedas.java
@@ -0,0 +1,4 @@
+package br.com.alura.conversor;
+
+public record Moedas(double USD, double ARS, double BRL, double EUR, double CAD, double JPY) {
+}
